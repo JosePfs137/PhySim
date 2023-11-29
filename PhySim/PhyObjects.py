96fc9f33dd81fdb974de301c0798e5fc38591d33
Jose Pablo Flores Saldaña <jfs6132022a@gmail.com>
2023-11-29 12:32:55 -0600

    Added two classes 'Particles' and 'Gas' no comments in the code.

--- PhySim/PhyObjects.py
+++ PhySim/PhyObjects.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pygame
+from random import randint, random
 import os 
 
 class MassPoint:
@@ -59,6 +60,7 @@ class MassPoint:
                     wall (Wall object): The physim.Wall that could collide with the particle.
         '''
         #Since there are no diagonal walls, we only need to check vertical and horizontal collisions.
+        #For that purpose, we calculate the edges of the wall and particles.
 
         pLpoint = self.r[0] - self.radius
         pRpoint = self.r[0] + self.radius
@@ -70,15 +72,19 @@ class MassPoint:
         wUpoint = wall.r[1] - wall.height/2
         wDpoint = wall.r[1] + wall.height/2
 
+        #Now we can calculate if a particle's edge is after a wall edge.
         Roverlap = pRpoint - wLpoint
         Loverlap = wRpoint - pLpoint
         Doverlap = pDpoint - wUpoint
         Uoverlap = wDpoint - pUpoint
 
+        # With that we will register a collision in the X direction if both objects overlap on the horizontal axis.
         Xcollision = (0 < Roverlap) and (0 < Loverlap)
         Ycollision = (0 < Doverlap) and (0 < Uoverlap)
 
+        #A collision occurs when the objects overlap on both axes.
         if Ycollision and Xcollision:
+            #Now we want to check the direction of the collision, for that, we look in which direction there is more overlap. (if it's equal the particle hit a corner)
             if min(Roverlap, Loverlap) >= min(Doverlap, Uoverlap):
                 d = self.r[1] - wall.r[1]
                 d = d/abs(d)
@@ -101,16 +107,25 @@ class MassPoint:
         #We named d the distance between both particles.
         d = p2.r - self.r
         d_len = np.sqrt(np.dot(d, d))
+
         #If the particles overlap:
         if np.sqrt(np.dot(d, d)) < self.radius + p2.radius:
-            #We calculate the final velocity assuming that the collision is elastic, therefore, 
-            #the energy and momentum right before the collision are the same as the energy and 
-            # momentum after the hit.
+            #When the particles collide, they overlap, and we need to avoid that, the solution 
+            #for that is changing their positions without moving the center of mass. Here the 
+            #displacement is taken in the difference vector direction and has a magnitude of one radius.
+            if d_len == 0:
+                theta = 2* np.pi * random()
+                d_unit = np.array([np.cos(theta), np.sin(theta)])
+            else:
+                d_unit = d/d_len
 
-            d_unit = d/d_len
             midpoint = d*(1/2) + self.r
             self.r = midpoint - d_unit*self.radius
             p2.r = midpoint + d_unit*p2.radius
+
+            #We calculate the final velocity assuming that the collision is elastic, therefore, 
+            #the energy and momentum right before the collision are the same as the energy and 
+            # momentum after the hit.
             
             c1 = (self.m - p2.m)/(self.m + p2.m)
             c2 = 2*self.m / (self.m + p2.m)
@@ -151,16 +166,147 @@ class Wall:
         rectangle = pygame.Rect(self.r[0] - self.width/2, self.r[1] - self.height/2, self.width, self.height)
         pygame.draw.rect(win, color, rectangle)
 
+class Particles:
+    def __init__ (self, *P):
+        self.P = P
+        self.N = len(P)
+
+    def __iter__(self):
+        return iter(self.P)
+    
+    def __getitem__(self, index):
+        if isinstance(index, (list, tuple, np.ndarray)):
+            for p in self:
+                if (p.r[0] == index[0]) and (p.r[1] == index[1]):
+                    return p
+        elif np.issubdtype(type(index), np.integer):
+            return self.P[index]
+        
+        else:
+            raise TypeError(str(type(index)) + ' object not supported')
+    
+    def MeanRadius(self):
+        meanR = 0
+        for p in self:
+            meanR += p.radius
+        return meanR/self.N
+    
+    def intCoords(self, num, spacing = 0):
+        if spacing == 0:
+            spacing = 3*self.MeanRadius()
+        
+        return int(num/spacing)
+    
+    def hashCoords(self, r):
+        size = 2*self.N 
+        h = (r[0] * 92837111) ^ (r[1] * 689287499) ^ (283923481)
+        return abs(h)%(size)
+
+    def hashPos (self, r):
+        x1 = self.intCoords(r[0])
+        y1 = self.intCoords(r[1])
+        return self.hashCoords([x1, y1])
+    
+    def Hash(self):
+        size = 2*self.N
+        
+        cellStart = np.zeros(size + 1, dtype = int)
+        cellEntries = np.zeros(self.N, dtype = int)
+
+        for p in self.P:
+            h = self.hashPos(p.r)
+            cellStart[h] += 1
+
+        start = 0
+        for i in range(size):
+            start += cellStart[i]
+            cellStart[i] = start
+        
+        cellStart[size] = start
+
+        for i in range(self.N):
+            h = self.hashPos(self.P[i].r)
+            cellStart[h] -= 1
+            j = cellStart[h]
+            cellEntries[j] = i
+
+        self.cellStart = cellStart
+        self.cellEntries = cellEntries
+    
+    def query(self, p, maxDist):
+
+        r_0 = np.array([self.intCoords(p.r[0] - maxDist), self.intCoords(p.r[1] - maxDist)])
+        r_1 = np.array([self.intCoords(p.r[0] + maxDist), self.intCoords(p.r[1] + maxDist)])
+
+        querrySize = 0
+        querryIds = -np.ones(self.N, dtype = int)
+
+        for i in range(r_0[0], r_1[0] + 1):
+            for j in range(r_0[1], r_1[1] + 1):
+                h = self.hashCoords([i,j])
+                start = self.cellStart[h]
+                end = self.cellStart[h+1]
+
+                for k in range(start, end):
+                    if self.cellEntries[k] not in querryIds:
+                        querryIds[querrySize] = self.cellEntries[k]
+                        querrySize += 1
+        
+        return querryIds, querrySize
+    
+    def Collision(self):
+        checked = []
+        for i in range(self.N):
+            Ids, Size = self.query(self[i], 2*self[i].radius)
+            for k in range(Size):
+                j = Ids[k]
+                if ({i, j} not in checked) and (j != i):
+                    self[i].particle_collision(self[j])
+                    checked.append({i,j})
+
+
+        
+
+
+class Gas(Particles):
+    def __init__(self, r_0, N, m, energy, width, height, radius = 10, SCALE = 10):
+        self.Rcm = r_0
+        self.N = N
+        self.m = m
+        self.energy = energy
+        self.width = width
+        self.height = height
+        self.radius = radius
+        self.SCALE = SCALE
+
+        energies = np.random.rand(N)
+        energies = energy * energies/energies.sum()
+        vel = np.sqrt(2*energies)
+        P = []
+        VerticalLim = height/2 - radius
+        HorizontalLim = width/2 - radius
+        for i in range(N):
+            theta = 2* np.pi * random()
+            v = [vel[i]* np.cos(theta), vel[i]* np.sin(theta)]
+            r = [randint(int(r_0[0] - HorizontalLim), int(r_0[0] + HorizontalLim)), 
+                 randint(int(r_0[1] - VerticalLim), int(r_0[1] + VerticalLim))]
+            color = (10*randint(0,10), 10*randint(10,25), 10*randint(20,25))
+            P.append(MassPoint(r, v, m, radius= radius, color = color))
+        
+        Particles.__init__(self, *P)
+
+
+
 class Simulation: 
     '''
     The simulation itself, with the pygame window and the main function.
     '''
 
-    def __init__(self, particles = [], walls = [],size = (600, 600), FPS = 60, time_res = 1, backgound = (240,240,240), name = 'Simulation', TOP = True, BOTTOM = True, LEFT = True, RIGHT = True):
+    def __init__(self, P, particles = [], walls = [],size = (600, 600), FPS = 60, time_res = 1, backgound = (240,240,240), name = 'Simulation', TOP = True, BOTTOM = True, LEFT = True, RIGHT = True):
         '''
         We first save all variables needed for the simulation
             Inputs:
-                    particles (list of physim.MassPoint objects): All the particles in our simulation (empty by default).
+                    particles (A list of physim.MassPoint objects): All the particles in our simulation. (empty by default)
                     walls (list of physim.Wall objects): All the additional walls in our simulation (empty by default).
                     size (tuple of int): A (WIDTH, HEIGHT) tuple that defines the size of our windows.
                     FPS (int): The maximum frame rate at which the simulation will run.
@@ -169,7 +315,7 @@ class Simulation:
                     name (string): The caption of the window.
                     TOP, BOTTOM, LEFT, RIGHT (boolean): If true, the corresponding window border will act as a wall.
         '''
-        self.particles = particles
+        self.P = P
         self.walls = walls
         self.size = size
         self.FPS = FPS
@@ -178,6 +324,10 @@ class Simulation:
         self.name = name
 
         self.WIN = pygame.display.set_mode(size)
+
+        pygame.font.init()
+        self.font = pygame.font.SysFont('consolas', 24)
+
         self.borders = []
         if TOP:
             self.borders.append(Wall((size[0]/2, 0), size[0], 10))
@@ -188,12 +338,12 @@ class Simulation:
         if RIGHT:
             self.borders.append(Wall((size[0], size[1]/2), 10, size[1]))
     
-    def particles_collisions (self):
+    def naive_particles_collisions (self):
         '''
         Handles all the collisions between particles.
         '''
         #We make a list with len(self.particles) elements so we can iterate in each particle easily.
-        N = len(self.particles)
+        N = self.P.N
         N_list = range(N)
         #Now we're interested in checking only once the collision between particle i and 
         #particle j, we created a set for that purpose.
@@ -205,18 +355,18 @@ class Simulation:
             for j in N_list:
                 if (i == j) | (j in checked):
                     continue
-                self.particles[i].particle_collision(self.particles[j])
+                self.P[i].particle_collision(self.P[j])
             checked.add(i)
     
     def wall_collisons (self):
         '''
         Handles all the collisions between particles and walls.
         '''
-        for p in self.particles:
+        for p in self.P:
             for wall in self.walls:
                 p.wall_collision(wall)
         
-        for p in self.particles:
+        for p in self.P:
             for wall in self.borders:
                 p.wall_collision(wall)
 
@@ -233,12 +383,12 @@ class Simulation:
         for wall in self.borders:
             wall.draw(self.WIN, (13, 20, 28))
         
-        for p in self.particles:
+        for p in self.P:
             p.draw(self.WIN)
         
         pygame.display.update()
 
-    def run(self):
+    def run(self, ShowFPS = False, naive = False):
         '''
         The main function in which we run the loop with the simulation.
         Here we're going to integrate all the functions we defined 
@@ -262,11 +412,22 @@ class Simulation:
                     break
             
             #Now we update the particles one by one.
-            for p in self.particles:
+            for p in self.P:
                 p.update(dt)
 
-            self.particles_collisions()
+            if naive:
+                self.naive_particles_collisions()
+
+            else:
+                self.P.Hash()
+                self.P.Collision()
+
             self.wall_collisons()
             self.draw()
+
+            if ShowFPS:
+                img = self.font.render('FPS: ' + str(int(clock.get_fps())), True, (0,0,0))
+                self.WIN.blit(img, (self.size[0] - 100, 20))
+                pygame.display.update()
 
 
